921f8dd7121aeb3289feb64f669f76da04c7bbc2
Heymorce <138555843+Heymorce@users.noreply.github.com>
2023-07-04 00:34:13 -0700

    Add files via upload

---
+++ list, turple and dictionary.py
@@ -0,0 +1,24 @@
+myFruitlist = ["apple", "banana", "cherry"]
+print(myFruitlist)
+print(type(myFruitlist))
+print(myFruitlist[2])
+myFruitlist[2]= "orange"
+print(myFruitlist)
+myFinalAnswerTuple = ("apple","banana","chery")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary= {
+    "akua" : "apple",
+    "sannvi" : "banana",
+    "paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["akua"])
+print(myFavoriteFruitDictionary["sannvi"])
+print(myFavoriteFruitDictionary["paulo"])
+
+myMixedTypeList = [45, 290578, 1.02, True, "my dog is on the bed" "45"]
